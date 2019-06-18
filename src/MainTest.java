a55fea3fe47dc98b8985de193dab59ce26762313
wqiu <qiuchao0705@hotmail.com>
2019-06-17 22:36:36 -0400

    formatted

--- src/MainTest.java
+++ src/MainTest.java
@@ -3,7 +3,7 @@ public class MainTest {
     public static void main(String[] args) {
 
 		System.out.println("hellohellosss");
-        System.out.println("Pika Pika :D");
+		System.out.println("Pika Pika :D");
 		System.out.println("hellohello");
 		System.out.println("world");
 		
