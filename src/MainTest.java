046b7a2d758166363b96cecfa4fa9f2f56cfb121
wqiu <qiuchao0705@hotmail.com>
2019-06-17 22:31:07 -0400

    updated maintest

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,7 +1,12 @@
 
 public class MainTest {
     public static void main(String[] args) {
+
 		System.out.println("hellohellosss");
-    System.out.println("Pika Pika :D");
+        System.out.println("Pika Pika :D");
+		System.out.println("hellohello");
+		System.out.println("world");
+		
+
 	}
 }
